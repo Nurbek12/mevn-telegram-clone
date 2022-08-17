1f46cd9184f13d705b61d5267557d7fbf0ff2bb4
Nurbek12 <84056469+Nurbek12@users.noreply.github.com>
2022-08-17 13:32:17 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const { Server } = require('socket.io')
 
 require('dotenv').config();
 
-const io = new Server(httpserver
-    // ,{cors: { origin: process.env.CLIENTSOCKETURL,credentials: true}}
-    )
+const io = new Server(httpserver, {cors: { origin: process.env.CLIENTSOCKETURL,credentials: true}})
 
 mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => { console.log('Database connected...') })
@@ -21,7 +19,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'upload')))
-app.use(express.static(path.join(__dirname, 'dist')))
+// app.use(express.static(path.join(__dirname, 'dist')))
 app.use(passport.initialize())
 
 require('./config/passport')(passport)
@@ -33,8 +31,8 @@ app.use('/message', require('./routes/message.js'))
 
 require('./config/socket')(io)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'dist','index.html'))
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname,'dist','index.html'))
+// });
 
 httpserver.listen(process.env.PORT, () => console.log('Server start...'))
